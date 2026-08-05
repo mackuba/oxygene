6d55db5f65e17f19799ced97d6abf54e94d75a55
Kuba Suder <jakub.suder@gmail.com>
2026-08-05 03:09:07 +0200

    created new gem from the template

---
+++ lib/oxygene.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "oxygene/version"
+
+module Oxygene
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/oxygene/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Oxygene
+  VERSION = "0.1.0"
+end
