85954f67ba106c725e45092417bc0157231ca9dc
Kuba Suder <jakub.suder@gmail.com>
2026-08-11 17:41:15 +0200

    renamed BASE32_PAIRS

--- lib/oxygene/base32.rb
+++ lib/oxygene/base32.rb
@@ -6,7 +6,7 @@ module Oxygene
   module Base32
     BASE32_ALPHABET = "abcdefghijklmnopqrstuvwxyz234567".freeze
 
-    BASE32_PAIRS = Array.new(1024) { |i|
+    BASE32_ENCODE_TABLE = Array.new(1024) { |i|
       (BASE32_ALPHABET.getbyte(i >> 5).chr + BASE32_ALPHABET.getbyte(i & 31).chr).freeze
     }.freeze
 
@@ -21,7 +21,7 @@ module Oxygene
       table.freeze
     end
 
-    private_constant :BASE32_ALPHABET, :BASE32_PAIRS, :BASE32_DECODE_TABLE
+    private_constant :BASE32_ALPHABET, :BASE32_ENCODE_TABLE, :BASE32_DECODE_TABLE
 
     def self.encode(data, start_offset = 0, prefix = "")
       total_size = data.bytesize
@@ -34,7 +34,7 @@ module Oxygene
       full_block_end = total_size - (size % 5)
 
       # A single base32 character is one of the 32 values in the BASE32_ALPHABET string
-      # above, i.e. 5 bits. The BASE32_PAIRS array stores a (flattened) 32x32 table
+      # above, i.e. 5 bits. The BASE32_ENCODE_TABLE array stores a (flattened) 32x32 table
       # of all possible combinations of two-character pairs (10 bits).
       #
       # Instead of taking the 40 bits of a 5-byte slice of the original string, slicing
@@ -49,29 +49,29 @@ module Oxygene
                 (data.getbyte(offset + 3) << 8) |
                 (data.getbyte(offset + 4))
 
-        output << BASE32_PAIRS[(value >> 30) & 1023]
-        output << BASE32_PAIRS[(value >> 20) & 1023]
-        output << BASE32_PAIRS[(value >> 10) & 1023]
-        output << BASE32_PAIRS[(value) & 1023]
+        output << BASE32_ENCODE_TABLE[(value >> 30) & 1023]
+        output << BASE32_ENCODE_TABLE[(value >> 20) & 1023]
+        output << BASE32_ENCODE_TABLE[(value >> 10) & 1023]
+        output << BASE32_ENCODE_TABLE[(value) & 1023]
 
         offset += 5
       end
 
       case total_size - offset
       when 1
-        output << BASE32_PAIRS[data.getbyte(offset) << 2]
+        output << BASE32_ENCODE_TABLE[data.getbyte(offset) << 2]
       when 2
         value = (data.getbyte(offset) << 8) | data.getbyte(offset + 1)
 
-        output << BASE32_PAIRS[value >> 6]
-        output << BASE32_PAIRS[(value & 63) << 4]
+        output << BASE32_ENCODE_TABLE[value >> 6]
+        output << BASE32_ENCODE_TABLE[(value & 63) << 4]
       when 3
         value = (data.getbyte(offset) << 16) |
                 (data.getbyte(offset + 1) << 8) |
                 (data.getbyte(offset + 2))
 
-        output << BASE32_PAIRS[value >> 14]
-        output << BASE32_PAIRS[(value >> 4) & 1023]
+        output << BASE32_ENCODE_TABLE[value >> 14]
+        output << BASE32_ENCODE_TABLE[(value >> 4) & 1023]
         output << BASE32_ALPHABET.getbyte((value & 15) << 1)
       when 4
         value = (data.getbyte(offset) << 24) |
@@ -79,9 +79,9 @@ module Oxygene
                 (data.getbyte(offset + 2) << 8) |
                 (data.getbyte(offset + 3))
 
-        output << BASE32_PAIRS[value >> 22]
-        output << BASE32_PAIRS[(value >> 12) & 1023]
-        output << BASE32_PAIRS[(value >> 2) & 1023]
+        output << BASE32_ENCODE_TABLE[value >> 22]
+        output << BASE32_ENCODE_TABLE[(value >> 12) & 1023]
+        output << BASE32_ENCODE_TABLE[(value >> 2) & 1023]
         output << BASE32_ALPHABET.getbyte((value & 3) << 3)
       end
 
